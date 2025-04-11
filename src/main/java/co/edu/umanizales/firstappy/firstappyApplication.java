833e71b92b59e431cd8d92062455832bb0ba0c60
María ISabel Galarza <mgalarza110544@umanizales.edu.co>
2025-04-10 23:20:51 -0500

    modificacion

--- src/main/java/co/edu/umanizales/firstappy/firstappyApplication.java
+++ src/main/java/co/edu/umanizales/firstappy/firstappyApplication.java
@@ -1,13 +1,16 @@
 package co.edu.umanizales.firstappy;
 
+import co.edu.umanizales.firstappy.model.Location;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstappyApplication {
+public class firstappyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FirstappyApplication.class, args);
+
+
+        SpringApplication.run(firstappyApplication.class, args);
     }
 
 }
